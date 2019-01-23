c13b06bd6bd298b9e792453ed48caf0d5bffb416
medical-boy-sf <medica_boy_sf@yahoo.com>
2019-01-23 20:09:29 +0200

    excellentGrade.js

---
+++ excellentGrade.js
@@ -0,0 +1,9 @@
+function excellentGrade (grade) {
+    grade = Number(grade);
+
+    if (grade >= 5.5) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
