4bf4efca45bd6ac665fe2acecb10374a129c095f
medical-boy-sf <medica_boy_sf@yahoo.com>
2019-01-24 10:36:56 +0200

    sumOfOddNumbers.js

---
+++ 11.sumOfOddNumbers.js
@@ -0,0 +1,11 @@
+function sumOfOddNumbers(n) {
+    n = Number(n);
+    let sum = 0;
+    for (let i = 1; i <= 2 * n; i += 2) {
+        console.log(i);
+        sum += i;
+    }
+    console.log(`Sum: ${sum}`);
+}
+
+sumOfOddNumbers(3);
